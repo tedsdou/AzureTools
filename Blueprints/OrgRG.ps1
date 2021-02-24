f864115a69930b0624e94f27913971c050a49cfc
tedsdou <61891016+tedsdou@users.noreply.github.com>
2021-02-24 17:00:56 -0600

    Create OrgRG.ps1

---
+++ Blueprints/OrgRG.ps1
@@ -0,0 +1,86 @@
+function Add-ResourceGroupName {
+    [CmdletBinding()]
+    param (
+        [ValidateSet('PROD','TEST','SANDBOX','QA')]
+        $Environment,
+        $AppName,
+        $ContactEmail,
+        $BillTo,
+        [ValidateSet('Critical','Key','Standard')]
+        $Criticality,
+        [ValidateSet( 'Confidential','Highly Sensitive','Internal','Public')]
+        $DataClass,
+        $Project,
+        $OrgShortName = 'DTE'
+    )
+    
+        If($Environment -eq 'PROD'){
+            $Location = 'CENTRALUS'
+        }else {
+            $Location = 'EASTUS2'
+        }
+        if ($Location -eq 'CENTRALUS') {
+            $RegionName = 'CU'
+        }else {
+            $RegionName = 'E2'
+        }
+        $AppRGName = $AppName.ToUpper()
+
+        $Tags =  @{
+            'BillTo' = $BillTo
+            'Application' = $AppName
+            'ContactEmail' = $ContactEmail
+            'BusinessCriticality' = $Criticality
+            'DataClassification' = $DataClass
+            'Project' = $Project
+          }
+          $Name = "$OrgShortName-$RegionName-$Environment-$AppRGName-RG"
+
+        #Build hash table to convert to json
+        @{
+            'properties' =  @{
+                'description' = ''
+                'targetScope' =  'subscription'
+                'parameters' = @{}
+                'resourceGroups' = @{
+                    "$OrgShortName`ResourceGroup" =  @{
+                        'description' =  "$OrgShortName Resource Group"
+                        "name" = $Name
+                        "tags" =  $Tags
+                        "location" = $location.ToLower()
+                    }
+                }
+            }
+        } 
+}
+
+try {
+    $null = Get-AzContext -ErrorAction Stop
+}
+catch {
+    Login-AzAccount
+}
+
+$params = @{
+        'Environment' = 'TEST'
+        'AppName' = 'TCU'
+        'ContactEmail' = 'WhiskeyTango@Foxtrot.com'
+        'BillTo' = 'Tango'
+        'Criticality' = 'Standard'
+        'DataClass' = 'Internal'
+        'Project' = 'ProjectTango'
+        'OrgShortName' = 'LAB'
+}
+$rg = Add-ResourceGroupName @params 
+$BluePrintPath = "C:\Blueprint\$($params.OrgShortName)-BluePrint"
+$BluePrintPath,"$BluePrintPath\artifacts" | ForEach-Object {
+    if (-not(Test-Path -Path $_ -ErrorAction ignore)) {
+        $null = New-Item -ItemType Directory -Path $_
+    }
+}
+
+$rg | ConvertTo-Json -Depth 100 | Out-File -FilePath "$BluePrintPath\blueprint.json" -Force
+If(-not(Get-Command -Name New-AzBlueprint -ListAvailable -ErrorAction SilentlyContinue)){Install-Module -Name Az.Blueprint -Force}
+
+#When you have everything populated in your $BluePrintPath\Artifacts directory, load it into Azure
+Import-AzBlueprintWithArtifact -Name "$($params.OrgShortName)-Blueprint" -SubscriptionId ((Get-AzContext).Subscription.Id) -InputPath $BluePrintPath
