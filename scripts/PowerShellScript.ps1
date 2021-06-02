0b9cc4be8d39b5dddf6a508893fcececffa228b1
tedsdou <61891016+tedsdou@users.noreply.github.com>
2021-06-02 14:31:56 -0500

    Delete scripts directory

--- scripts/PowerShellScript.ps1
+++
@@ -1,3 +0,0 @@
-Param($a,$b)
-
-Write-Output "`$a is $a`n`r`$b is $b" | out-file -filepath c:\scripts\PowerShellScriptOutput.log
--- scripts/get-GPOReport.ps1
+++
@@ -1,6 +0,0 @@
-
-$names = (Get-GPO -All).DisplayName
-foreach($n in $names){ 
-    $display = $n -replace 'd'
-    Get-GPOReport -ReportType Xml -Name $n -Path "$PSScriptRoot\$display.xml"  
-    }
