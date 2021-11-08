9b08b10b1959a47c5774af2a6a82b182aa1eecad
Ted Sdoukos <TedSdoukos@hotmail.com>
2021-11-08 14:33:48 -0600

    Added Disclaimers

--- AzVnetInformation.ps1
+++ AzVnetInformation.ps1
@@ -1,13 +1,30 @@
-$filepath = 'C:\Temp\vnetInfo.csv'
+<#
+.NOTES
+    DISCLAIMER:
+    ===========
+    This Sample Code is provided for the purpose of illustration only and is 
+    not intended to be used in a production environment.  
+    THIS SAMPLE CODE AND ANY RELATED INFORMATION ARE PROVIDED "AS IS" WITHOUT
+    WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT 
+    LIMITED TO THE IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS
+    FOR A PARTICULAR PURPOSE.  
 
-<# 
-I need to go deeper into 'VnetAddressPrefixes'   = (($VNET).AddressSpace.AddressPrefixes -join ' ')
-I want it to also discover available addresses. Usage?
-
-Find number of available IPs, like how it is in the gui
+    We grant You a nonexclusive, royalty-free
+    right to use and modify the Sample Code and to reproduce and distribute
+    the object code form of the Sample Code, provided that You agree:
+    (i) to not use Our name, logo, or trademarks to market Your software
+    product in which the Sample Code is embedded; (ii) to include a valid
+    copyright notice on Your software product in which the Sample Code is
+    embedded; and (iii) to indemnify, hold harmless, and defend Us and
+    Our suppliers from and against any claims or lawsuits, including
+    attorneys' fees, that arise or result from the use or distribution
+    of the Sample Code.
 #>
 
-#Login-AzAccount
+$filepath = 'C:\Temp\vnetInfo.csv'
+If(-Not(Get-AzContext)){
+  Connect-AzAccount
+}
 $Subscription = Get-AzSubscription | Where-Object {$_.Name -match 'tgs|visual'} 
 foreach ($S in $Subscription) {
   $null = Set-AzContext -SubscriptionName $S.Name
