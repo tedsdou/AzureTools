9e6154e95c2811c24fd433d6bf68beeea3fd77c8
Ted Sdoukos <TedSdoukos@hotmail.com>
2023-02-27 13:12:41 -0600

    Updated Disclaimer

--- AzTagReport.ps1
+++ AzTagReport.ps1
@@ -19,25 +19,27 @@ function Edit-AzTag {
         Date:    4NOV2020
         Version: 1.0
        
-        DISCLAIMER:
-        ===========
-        This Sample Code is provided for the purpose of illustration only and is 
-        not intended to be used in a production environment.  
-        THIS SAMPLE CODE AND ANY RELATED INFORMATION ARE PROVIDED "AS IS" WITHOUT
-        WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT 
-        LIMITED TO THE IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS
-        FOR A PARTICULAR PURPOSE.  
+        Copyright (c) Microsoft Corporation.
 
-        We grant You a nonexclusive, royalty-free
-        right to use and modify the Sample Code and to reproduce and distribute
-        the object code form of the Sample Code, provided that You agree:
-        (i) to not use Our name, logo, or trademarks to market Your software
-        product in which the Sample Code is embedded; (ii) to include a valid
-        copyright notice on Your software product in which the Sample Code is
-        embedded; and (iii) to indemnify, hold harmless, and defend Us and
-        Our suppliers from and against any claims or lawsuits, including
-        attorneys' fees, that arise or result from the use or distribution
-        of the Sample Code.
+        MIT License
+
+        Permission is hereby granted, free of charge, to any person obtaining a copy
+        of this software and associated documentation files (the "Software"), to deal
+        in the Software without restriction, including without limitation the rights
+        to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+        copies of the Software, and to permit persons to whom the Software is
+        furnished to do so, subject to the following conditions:
+
+        The above copyright notice and this permission notice shall be included in all
+        copies or substantial portions of the Software.
+
+        THE SOFTWARE IS PROVIDED *AS IS*, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+        IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+        FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+        AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+        LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+        OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+        SOFTWARE.
     #>
     [CmdletBinding(DefaultParameterSetName = 'Targeted')]
     param (
@@ -75,7 +77,7 @@ function Edit-AzTag {
                 If ($ResourceGroup) {
                     try {
                         [array]$Resource = Get-AzResourceGroup -Name $ResourceGroup -ErrorAction Stop | Where-Object { $_.Tags.Keys -contains $b.Name } | 
-                        Select-Object -Property Tags, ResourceId
+                            Select-Object -Property Tags, ResourceId
                         $Resource += Get-AzResource -ResourceGroupName $ResourceGroup -TagName $b.Name | Select-Object -Property Tags, ResourceId
                     }
                     catch {
