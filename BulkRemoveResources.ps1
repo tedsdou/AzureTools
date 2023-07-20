4c95648d671d53a3e74d033e835b5d800aea2caf
Ted Sdoukos <TedSdoukos@hotmail.com>
2023-07-20 09:14:37 -0500

    New File Upload

---
+++ BulkRemoveResources.ps1
@@ -0,0 +1,23 @@
+$VMList = 'Comma-Separated List of VMs'
+$RGList = 'Comma-Separated List of RGs'
+$TenantID = '<TenantID>'
+
+Login-AzAccount -Tenant $TenantID
+
+$VMList | ForEach-Object -Parallel {
+    $vmConfig = Get-AzVM $_
+    $vmConfig.StorageProfile.OsDisk.DeleteOption = 'Delete'
+    $vmConfig.StorageProfile.DataDisks | ForEach-Object { $_.DeleteOption = 'Delete' }
+    $vmConfig.NetworkProfile.NetworkInterfaces | ForEach-Object { $_.DeleteOption = 'Delete' }
+    $vmConfig | Update-AzVM
+    Remove-AzVM -Name $_ -ResourceGroupName $vmConfig.ResourceGroupName -WhatIf
+}
+
+foreach ($RG in $RGList) {
+    Get-AzResource -ResourceGroupName $RG | ForEach-Object -Parallel {
+        Remove-AzResource -ResourceId $_.ResourceId -Force
+    }
+    Remove-AzResourceGroup -Name $RG
+}
+
+
